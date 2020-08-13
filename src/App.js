35c85280fcaf4ac9134673a4625c490612515ebe
Ozgur Akbaba <hello@ozgurakbaba.com>
2020-08-12 23:52:54 -0700

    amplify auth added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
 function App() {
   return (
     <div className="App">
       <div className="App-header">
         <h1>AWS Amplify</h1>
+        <AmplifySignOut />
       </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
