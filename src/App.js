438774ff62deb6abc370c9aee5385841a6e3a9e8
Ozgur Akbaba <hello@ozgurakbaba.com>
2020-08-14 17:54:19 -0700

    testing build error

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <div className="App-header">
         <h1>AWS Amplify</h1>
+        <p>Testing build on AWS Amplify</p>
         <AmplifySignOut />
       </div>
     </div>
